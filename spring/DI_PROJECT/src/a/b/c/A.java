324c8943dee4f0750fa15f755ba20d4e711f68ba
kakaru <kakaru1331@gmail.com>
2018-04-04 17:46:36 +0900

    04-04 spring practice

---
+++ spring/DI_PROJECT/src/a/b/c/A.java
@@ -0,0 +1,20 @@
+package a.b.c;
+
+public class A {
+	
+	private B b;
+	private C c;
+	
+	A (B bDummy) {
+		b = bDummy;
+	}
+	
+	void setC(C cDummy) {
+		c = cDummy;
+	}
+	
+//	void doIt() {
+//		B b = new B();
+//	}
+	
+}
