16dfa50768b2878036becd5c8e7c27e31c5a8a66
kakaru <kakaru1331@gmail.com>
2018-04-10 17:51:24 +0900

    04-10 request param

--- spring/MVC_PROJECT/src/main/java/a/b/c/auth/controller/LoginController.java
+++ spring/MVC_PROJECT/src/main/java/a/b/c/auth/controller/LoginController.java
@@ -1,11 +1,14 @@
 package a.b.c.auth.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
@@ -17,14 +20,21 @@ public class LoginController {
 	}
 	
 	@RequestMapping(value="/auth/login", method=RequestMethod.POST)
-	public String login(HttpServletRequest req) {		
-		String inputID = req.getParameter("id");
-		String inputPassword = req.getParameter("password");
+	public String login(HttpServletRequest req, 
+			@RequestParam("id") String id,
+			@RequestParam("password") String pw,
+			@RequestParam("hobby") List hobbies,			
+			HttpSession session) {
+		
+//		String inputID = req.getParameter("id");
+//		String inputPassword = req.getParameter("password");
+		String inputID = id;
+		String inputPassword = pw;
 		Boolean authenticateResult;		
 		
 		authenticateResult = authenticate(inputID, inputPassword);		
 		if (authenticateResult) {
-			HttpSession session = req.getSession();
+//			HttpSession session = req.getSession();
 			session.setAttribute("isLogin", "true");
 			session.setAttribute("id", inputID);
 			
--- spring/MVC_PROJECT/src/main/webapp/WEB-INF/views/auth/loginP.jsp
+++ spring/MVC_PROJECT/src/main/webapp/WEB-INF/views/auth/loginP.jsp
@@ -11,7 +11,10 @@
 	<form action="/auth/login" method="post">
 		<input type="text" name="id" placeholder="아이디">
 		<input type="password" name="password" placeholder="비밀번호">
-		<input type="submit" value="로그인">
+		<input type="checkbox" name="hobby" value="p">프로그래밍
+		<input type="checkbox" name="hobby" value="r">독서
+		<input type="checkbox" name="hobby" value="s">스포츠		
+		<input type="submit" value="로그인">		
 	</form>	
 	
 </body>
